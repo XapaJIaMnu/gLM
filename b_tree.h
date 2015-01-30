8f49fffe5a9e907e945ca859a3fe8af7e99ad780
XapaJIaMnu <nheart@gmail.com>
2015-01-30 16:54:21 +0000

    Double free or corruption

--- b_tree.h
+++ b_tree.h
@@ -12,15 +12,19 @@ class B_tree_node {
         bool is_leaf; //Maybe could replace with a a check for NULL pointer below
         B_tree_node ** children;
         B_tree_node * parent; //Backpointer to parent node
+
+        //Operations
         B_tree_node(unsigned short);
         ~B_tree_node();
         void insert_entry(Entry new_entry, unsigned short position); //Inserting in the keys array. Should be called only if there is space.
         void split();
+        void insert_child(B_tree_node * new_child, unsigned short position);
 };
 
 B_tree_node::B_tree_node(unsigned short min_degree) {
     num_keys = 0;
     keys = nullptr;// new Entry[2*min_degree - 1]; //Maximum number of entries;
+    parent = nullptr;
     is_leaf = false;
     num_children = 2*min_degree;
     children = new B_tree_node *[num_children]; //Maximum number of child nodes
@@ -49,6 +53,7 @@ class B_tree{
         ~B_tree(); //Destructor.
         void insert_Entry(Entry& new_entry);
         std::pair<B_tree_node *, unsigned short> findPosition(unsigned short value);
+        void draw_tree();
 };
 
 B_tree::B_tree (unsigned short min_degree){
@@ -94,27 +99,63 @@ std::pair<B_tree_node *, unsigned short> B_tree::findPosition(unsigned short val
 }
 
 void B_tree::insert_Entry (Entry& new_entry) {
-    //Find a location and insert a new entry there.
-    //First find the position we need
+    std::pair<B_tree_node *, unsigned short> location = findPosition(new_entry.value);
+    location.first->insert_entry(new_entry, location.second);
+
+    if (location.first -> num_keys > location.first->max_keys) {
+        location.first->split();
+    }
+
 
 }
 
 void B_tree_node::insert_entry (Entry new_entry, unsigned short position) {
     //Create a new array and deallocate the old one including the new entry inside;
     Entry * new_arr = new Entry[num_keys + 1];
-    for (unsigned short i = 0; i<num_keys; i++) {
-        if (i < position) {
-            new_arr[i] = keys[i];
-        } else if (i == position) {
-            new_arr[i] = new_entry;
-            new_arr[i+1] = keys[i];
-        } else {
-            new_arr[i+1] = keys[i];
+
+    if (num_keys == 0) {
+        new_arr[0] = new_entry;
+    } else {
+
+        for (unsigned short i = 0; i<num_keys; i++) {
+            if (i < position) {
+                new_arr[i] = keys[i];
+            } else if (i == position) {
+                new_arr[i] = new_entry;
+                new_arr[i+1] = keys[i];
+            } else {
+                new_arr[i+1] = keys[i];
+            }
         }
     }
+
     delete[] keys; //Free the memory taken by the old array
     keys = new_arr;
     num_keys++;
+}
+
+void B_tree_node::insert_child (B_tree_node * new_child, unsigned short position) {
+    B_tree_node ** children_new = new B_tree_node*[num_children +1];
+
+    if (num_children == 0) {
+        children_new[0] = new_child;
+    } else {
+
+        for (unsigned short i =0; i < num_children; i++) {
+            if (i < position) {
+                children_new[i] = children[i];
+            } else if (i == position) {
+                children_new[i] = new_child;
+                children_new[i+1] = children[i];
+            } else {
+                children_new[i+1] = children[i];
+            }
+        }
+    }
+
+    delete[] children;
+    children = children_new;
+    num_children++;
 }
 
 void B_tree_node::split(){
@@ -144,8 +185,6 @@ void B_tree_node::split(){
     }
     right_node->num_children = num_children/2;
 
-    right_node->parent = parent; //Set the parent. same as the current node.
-
     //Trim the left node to the previous size. Only need to change size, 
     //We don't care for slightly higher memory usage
     //WE DO CARE, need to deallocate it. Create new arrays;
@@ -173,12 +212,58 @@ void B_tree_node::split(){
     keys = keys_tmp;
     children = children_tmp;
 
-    parent->insert_entry(entry_to_move, parent->num_keys/2);
-    //Check if splitting is necesssary.
-    if (num_keys > max_keys) {
-        parent->split();
+    //Parent might be a nullptr here. We might need to add a new root node.
+    if (parent) {
+        right_node->parent = parent; //Set the parent. same as the current node.
+        //Insert entry in parent and put a link to the new child
+        parent->insert_entry(entry_to_move, parent->num_keys/2);
+        parent->insert_child(right_node, parent->num_keys/2 + 1);
+        //Check if splitting is necesssary.
+        if (parent->num_keys > parent->max_keys) {
+            parent->split();
+        }
+
+    } else {
+        //We are a root node and we have run out of space. We need to create a new root.
+        //In order to avoid reasigning a root node in the tree, create a new node and copy
+        //the left side onto it
+        B_tree_node * left_node = new B_tree_node(max_keys);
+        left_node->children = children;
+        left_node->keys = keys;
+        left_node->parent = this;
+        left_node->num_keys = num_keys;
+        left_node->num_children = num_children;
+
+        //Now make the node contain only the single key
+        delete[] children;
+        delete[] keys;
+        num_children = 0;
+        num_keys = 0;
+        this->insert_entry(entry_to_move, 0);
+        this->insert_child(left_node, 0);
+        this->insert_child(right_node, 1);
     }
 
+
+}
+
+void print_node(B_tree_node * node) {
+    std::cout << "Node_ID" << node << std::endl;
+    std::cout << "Num keys: " << node->num_keys;
+    std::cout << "Num children: " << node-> num_children;
+    std::cout << "Values: ";
+    for (unsigned short i = 0; i<node->num_keys; i++) {
+        std::cout << node->keys[i].value << ' ';
+    }
+    for (unsigned short i = 0; i<node->num_children; i++) {
+        print_node(node->children[i]);
+    }
+
+}
+
+void B_tree::draw_tree() {
+    //Draws what is currently in the tree. For debugging purposes
+    print_node(root_node);
 }
 
 B_tree::~B_tree(){
--- testing.cpp
+++ testing.cpp
@@ -1,6 +1,27 @@
 #include "b_tree.h"
 
 int main() {
-    B_tree * pesho = new B_tree(5);
+    B_tree * pesho = new B_tree(2);
+    Entry A = {10, nullptr, false};
+    Entry B = {15, nullptr, false};
+    Entry D = {16, nullptr, false};
+    Entry E = {17, nullptr, false};
+    Entry F = {18, nullptr, false};
+    Entry G = {19, nullptr, false};
+    Entry H = {22, nullptr, false};
+    Entry I = {35, nullptr, false};
+
+    pesho->insert_Entry(A);
+    pesho->insert_Entry(B);
+    std::cout << "Here" << std::endl;
+    pesho->insert_Entry(D);
+    pesho->insert_Entry(E);
+    std::cout << "Here too" << std::endl;
+    pesho->insert_Entry(F);
+    pesho->insert_Entry(G);
+    pesho->insert_Entry(H);
+    pesho->insert_Entry(I);
+    //pesho->insert_Entry(A);
+    pesho->draw_tree();
     delete pesho;
 }
