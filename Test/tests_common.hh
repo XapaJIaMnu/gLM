56f8806a1f418b319c0457ca9069151dc1bb3c1f
Nikolay Bogoychev <nheart@gmail.com>
2015-09-06 21:52:28 +0100

    Forgot to add some new test files

---
+++ Test/tests_common.hh
@@ -0,0 +1,10 @@
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE Suites
+#define FLOAT_TOLERANCE 1e-5*1e-5
+#include <boost/test/unit_test.hpp> 
+
+//Float comparison
+inline bool float_compare(float a, float b) { 
+
+    return (a - b) * (a - b) < FLOAT_TOLERANCE;
+}
