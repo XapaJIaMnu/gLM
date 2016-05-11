60f41a9142b8139d58889fe9aa95bf3bc912c65f
Nikolay Bogoychev <nheart@gmail.com>
2016-05-11 16:32:29 +0100

    Build release by default

--- gpu/memory_management.cu
+++ gpu/memory_management.cu
@@ -1,30 +1,41 @@
 #include "gpu_common.h"
 #include "memory_management.hh"
 
-unsigned char * copyToGPUMemory(unsigned char * byte_arr, unsigned int byte_arr_size) {
+unsigned char * copyToGPUMemory(unsigned char * byte_arr, size_t byte_arr_size) {
     unsigned char * gpu_byte_arr;
     CHECK_CALL(cudaMalloc(&gpu_byte_arr, byte_arr_size*sizeof(unsigned char)));
     CHECK_CALL(cudaMemcpy(gpu_byte_arr, byte_arr, byte_arr_size*sizeof(unsigned char), cudaMemcpyHostToDevice));
     return gpu_byte_arr;
 }
 
-unsigned int * copyToGPUMemory(unsigned int * byte_arr, unsigned int byte_arr_size) {
+unsigned int * copyToGPUMemory(unsigned int * byte_arr, size_t num_elements) {
     unsigned int * gpu_byte_arr;
-    CHECK_CALL(cudaMalloc(&gpu_byte_arr, byte_arr_size*sizeof(unsigned int)));
-    CHECK_CALL(cudaMemcpy(gpu_byte_arr, byte_arr, byte_arr_size*sizeof(unsigned int), cudaMemcpyHostToDevice));
+    CHECK_CALL(cudaMalloc(&gpu_byte_arr, num_elements*sizeof(unsigned int)));
+    CHECK_CALL(cudaMemcpy(gpu_byte_arr, byte_arr, num_elements*sizeof(unsigned int), cudaMemcpyHostToDevice));
     return gpu_byte_arr;
 }
 
-void allocateGPUMem(size_t size, unsigned int ** gpu_mem) {
-    CHECK_CALL(cudaMalloc(gpu_mem, size*sizeof(unsigned int)));
+void copyToGPUMemoryNoAlloc(unsigned int * gpuMem, unsigned int * input, size_t num_elements) {
+
+    CHECK_CALL(cudaMemcpy(gpuMem, input, num_elements*sizeof(unsigned int), cudaMemcpyHostToDevice));
 }
 
-void allocateGPUMem(size_t size, float ** gpu_mem) {
-    CHECK_CALL(cudaMalloc(gpu_mem, size*sizeof(float)));
+void copyToGPUMemoryNoAlloc(float * gpuMem, float * input, size_t num_elements) {
+
+    CHECK_CALL(cudaMemcpy(gpuMem, input, num_elements*sizeof(float), cudaMemcpyHostToDevice));
 }
 
-void copyToHostMemory(float * gpu_mem, float * cpu_mem, size_t size) {
-    CHECK_CALL(cudaMemcpy(cpu_mem, gpu_mem, size*sizeof(float), cudaMemcpyDeviceToHost));
+
+void allocateGPUMem(size_t num_elements, unsigned int ** gpu_mem) {
+    CHECK_CALL(cudaMalloc(gpu_mem, num_elements*sizeof(unsigned int)));
+}
+
+void allocateGPUMem(size_t num_elements, float ** gpu_mem) {
+    CHECK_CALL(cudaMalloc(gpu_mem, num_elements*sizeof(float)));
+}
+
+void copyToHostMemory(float * gpu_mem, float * cpu_mem, size_t num_elements) {
+    CHECK_CALL(cudaMemcpy(cpu_mem, gpu_mem, num_elements*sizeof(float), cudaMemcpyDeviceToHost));
 }
 
 void freeGPUMemory(unsigned char * gpu_ptr) {
--- gpu/memory_management.hh
+++ gpu/memory_management.hh
@@ -1,12 +1,20 @@
 #include <stddef.h>
 
-unsigned char * copyToGPUMemory(unsigned char * byte_arr, unsigned int byte_arr_size);
+unsigned char * copyToGPUMemory(unsigned char * byte_arr, size_t byte_arr_size);
 void freeGPUMemory(unsigned char * gpu_ptr);
 
-unsigned int * copyToGPUMemory(unsigned int * byte_arr, unsigned int byte_arr_size);
+unsigned int * copyToGPUMemory(unsigned int * byte_arr, size_t num_elements);
+
+void copyToGPUMemoryNoAlloc(unsigned int * gpuMem, unsigned int * input, size_t num_elements);
+
+void copyToGPUMemoryNoAlloc(float * gpuMem, float * input, size_t num_elements);
+
+
 void freeGPUMemory(unsigned int * gpu_ptr);
 
 void freeGPUMemory(float * gpu_ptr);
 
-void allocateGPUMem(size_t size, float ** gpu_mem);
-void copyToHostMemory(float * gpu_mem, float * cpu_mem, size_t size);
+void allocateGPUMem(size_t num_elements, unsigned int ** gpu_mem);
+void allocateGPUMem(size_t num_elements, float ** gpu_mem);
+void copyToHostMemory(float * gpu_mem, float * cpu_mem, size_t num_elements);
+void copyToHostMemory(unsigned int * gpu_mem, unsigned int * cpu_mem, size_t num_elements);
